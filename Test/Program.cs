b8e2f2d273e19ac8f76ae07a5d6140b1c6570f0e
«dima» <dima-titov-93@mail.ru>
2022-05-09 13:44:47 +0300

    добавление допустимых строк в новый массив

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,20 +1,39 @@
-﻿public class ContolTest
+﻿public class ContorlTest 
 {
-     static void Main(string[] args)
-     {
-         string[] array = new string[] {"Dima", "One", "GB", "study", "test"};
+    static void Main(string[] args)
+    {
+        string[] Array = new string[] {"Dima", "One", "GB", "study", "test", "two", "win"};
 
-     }
+        string[] newArray = CreateNewArray(Array); 
 
-     public static string[] NewArray (string[] array)
-     {
-         string[] NewArray;
-         int count =0;
-         for (int i = 0; i < array.Length; i++)
-         {
-             if(array[i].Length<=3)
-             count++;
+        Console.WriteLine(String.Join(" ", newArray)); 
+    }
 
-         }
-     }
+    public static string[] CreateNewArray (string[] array) 
+    {
+        string[] newArray;
+
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++) 
+        {
+            if (array[i].Length <= 3) 
+            { 
+                count++; 
+            }
+        }
+
+        newArray = new string[count]; 
+        count = 0;
+
+        for (int i = 0; i < array.Length; i++) 
+        {
+            if (array[i].Length <= 3) 
+            {
+                newArray[count++] = array[i];
+            }
+        }
+
+        return newArray; 
+    }
 }
