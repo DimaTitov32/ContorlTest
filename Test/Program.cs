942668cb5fe86df31b7577b0f0ad533943d29ed7
«dima» <dima-titov-93@mail.ru>
2022-05-09 13:17:05 +0300

    подсчитываем количество строк длины <= 3

--- Test/Program.cs
+++ Test/Program.cs
@@ -5,5 +5,16 @@
          string[] array = new string[] {"Dima", "One", "GB", "study", "test"};
 
      }
-     
+
+     public static string[] NewArray (string[] array)
+     {
+         string[] NewArray;
+         int count =0;
+         for (int i = 0; i < array.Length; i++)
+         {
+             if(array[i].Length<=3)
+             count++;
+
+         }
+     }
 }
